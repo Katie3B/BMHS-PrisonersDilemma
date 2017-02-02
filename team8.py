07b0c6325c5c102fd1d0ab293d6bb9e45a817d24
Katie3B <kathkoer22361@ccps.org>
2017-02-02 13:36:17 -0500

    rtyha

--- team8.py
+++ team8.py
@@ -6,10 +6,11 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'Team 5' # Only 10 chars displayed.
-strategy_name = 'The Best Strategy'
-strategy_description = 'How does this strategy decide?'
-    #If you see this then it worked
+team_name = 'Guambabwe' # Only 10 chars displayed.
+strategy_name = 'Blitzkrieg'
+strategy_description = 'Destroying the other teams with full force.'
+
+
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
@@ -17,7 +18,69 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
+    '''Count points here'''
+    if my_history == 'c' and their_history == 'c':
+        my_score += 650
+        
+    if their_history == 'b' and my_history == 'b':
+        my_score -= 250
+        
+    if my_history == 'b' and their_history == 'c':
+        my_score += 100
+        
+    if my_history == 'c' and their_history == 'b':
+        my_score -= 500
+        
+    '''Stop counting points here'''
+    
+    '''first five rounds'''
+    if my_history == '': # This block will add variables and initialize their values for the first turn
+        number_of_turns = 0
+        my_score = 0
+        enemy_betray_streak = 0
+        enemy_collude_streak = 0
+        
+    if their_history == 'b': #Adds the streak function
+        enemy_betray_streak += 1
+        enemy_collude_streak = 0
+        
+    if their_history == 'c':
+        enemy_collude_streak += 1
+        enemy_betray_streak = 0
+        
+    if (number_of_turns % 5 == 0):
+        rando_choice = random.randint(1,2)
+        if rando_choice == 1:
+            return 'b'
+        elif rando_choice == 2:
+            return 'c'
+        else:
+            return 'b'
+    elif their_history == 'b' and enemy_betray_streak > 2:
+        number_of_turns += 1
+        return 'b'
+    elif their_history == 'c' and enemy_collude_streak > 3:
+        number_of_turns += 1
+        return 'b'
+    else:
+        number_of_turns += 1
+        return 'b' 
+        
+        
+        
+    if number_of_turns == 1 or number_of_turns == 2: #First five turns
+        number_of_turns +=1
+        return 'b'
+    elif number_of_turns == 3 or number_of_turns == 4:
+        number_of_turns += 1
+        return 'c'
+    if their_history == '':
+        number_of_turns += 1
+        return 'b'
+        
+    
+        
+    
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -25,44 +88,3 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
-
-    
-def test_move(my_history, their_history, my_score, their_score, result):
-    '''calls move(my_history, their_history, my_score, their_score)
-    from this module. Prints error if return value != result.
-    Returns True or False, dpending on whether result was as expected.
-    '''
-    real_result = move(my_history, their_history, my_score, their_score)
-    if real_result == result:
-        return True
-    else:
-        print("move(" +
-            ", ".join(["'"+my_history+"'", "'"+their_history+"'",
-                       str(my_score), str(their_score)])+
-            ") returned " + "'" + real_result + "'" +
-            " and should have returned '" + result + "'")
-        return False
-
-if __name__ == '__main__':
-     
-    # Test 1: Betray on first move.
-    if test_move(my_history='',
-              their_history='', 
-              my_score=0,
-              their_score=0,
-              result='b'):
-         print 'Test passed'
-     # Test 2: Continue betraying if they collude despite being betrayed.
-    test_move(my_history='bbb',
-              their_history='ccc', 
-              # Note the scores are for testing move().
-              # The history and scores don't need to match unless
-              # that is relevant to the test of move(). Here,
-              # the simulation (if working correctly) would have awarded 
-              # 300 to me and -750 to them. This test will pass if and only if
-              # move('bbb', 'ccc', 0, 0) returns 'b'.
-              my_score=0, 
-              their_score=0,
-              result='b')             
