ef02772b76749220e738805987c8188e41db7809
WConrad1 <wyatconr20119@ceciltso.org>
2017-02-02 13:08:00 -0500

    Update

--- team4.py
+++ team4.py
@@ -1,17 +1,6 @@
-####
-# Each team's file must define four tokens:
-#     team_name: a string
-#     strategy_name: a string
-#     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
-
-
-# I added this line here
-####
-
-team_name = 'IHateMatt' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Matt and Wyatt' # Only 10 chars displayed.
+strategy_name = 'Hope for the best'
+strategy_description = 'whatever they dont do'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -20,12 +9,15 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these tuhygtfrdeswwo players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
+    if(their_history != ""):
+        theirLast = their_history[len(their_history) - 1]
+        
+        
+        if(theirLast == "b"):
+            return "c"
+        else:
+            return "b"
+    else:
+        return "c"
     
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
     
